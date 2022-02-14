1dbddacf0612c975cc9df1422f049340e84498e0
Benjohn Barnes <benjohn@fysh.org>
2022-02-14 06:44:58 +0000

    Add test of isolation

--- Tests/NestTestsTests/NestedTestTests.swift
+++ Tests/NestTestsTests/NestedTestTests.swift
@@ -3,10 +3,44 @@ import XCTest
 
 final class NestedTestTests: XCTestCase {
 
-    func testNestedTest() throws {
+    func testNestedTestsRunInIsolation() throws {
+
+        try nestTests("nested test run in isolation") {
+
+            var events = Array<String>()
+            events.append("root")
+            
+            return .nest("nest1") {
+                events.append("nest1")
+                
+                return .leaf("leaf11") {
+                    events.append("leaf11")
+                    XCTAssertEqual(events, ["root", "nest1", "leaf11"])
+                }
+                .leaf("leaf12") {
+                    events.append("leaf12")
+                    XCTAssertEqual(events, ["root", "nest1", "leaf12"])
+                }
+            }
+            .nest("nest 2") {
+                events.append("nest2")
+                
+                return .leaf("leaf21") {
+                    events.append("leaf21")
+                    XCTAssertEqual(events, ["root", "nest2", "leaf21"])
+                }
+                .leaf("leaf22") {
+                    events.append("leaf22")
+                    XCTAssertEqual(events, ["root", "nest2", "leaf22"])
+                }
+            }
+        }
+    }
+
+    func testAllNestedTestsAreRun() throws {
         var log = Array<String>()
 
-        try nestTests("some simple test") {
+        try nestTests("all nested tests are run") {
             log.append("root")
             
             return .nest("nest1") {
